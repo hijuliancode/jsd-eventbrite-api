c746d851175ef6ca405ee51467e6cbb5446b5f31
JulianCode <hijuliansosa@protonmail.com>
2020-05-22 19:18:55 -0500

    Making requests to the REST API of events

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const ui = new Interfaz();
 
 // Variables
 const searchButton = document.getElementById('buscarBtn');
-const searchText = document.getElementById('evento');
+const searchTextInput = document.getElementById('evento');
 const selectCategories = document.getElementById('listado-categorias')
 
 // Listeners
@@ -13,16 +13,16 @@ searchButton.addEventListener('click', buttonSearch);
 // Functions
 function buttonSearch(e) {
   e.preventDefault()
-  const eventToSearch = searchText.value;
+  const searchText = searchTextInput.value;
   const categorieSelected = selectCategories.options[selectCategories.selectedIndex].value;
-  
-  if(eventToSearch !== '') {
-    // console.log(categorieSelected)
-    console.log('Buscando...');
-    
-    searchText.value = '';
+  if(searchText !== '') {
+    eventbrite.getEvents(searchText, categorieSelected)
+      .then(data => {
+        console.log('=> => =>', data);
+      })
+      searchTextInput.value = '';
   } else {
     ui.showMessage('Escribe algo en el buscador', 'alert alert-danger mt-3');
   }
-  searchText.focus();
+  searchTextInput.focus();
 }
--- js/eventbrite.js
+++ js/eventbrite.js
@@ -15,4 +15,15 @@ class EventBrite {
       categories
     };
   }
+  async getEvents(event, category) {
+    // Consulta los eventos a la REST API de eventbrite
+    const respEvents = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${event}&sort_by=${this.order}&categories=${category}&token=${this.token_oauth}`);
+
+    // Esperar la respuesta de los eventos y devolver un JSON
+    const events = await respEvents.json()
+
+    return {
+      events
+    };
+  }
 }
